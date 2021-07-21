3a657854f11b0c03fc9111136166c612ca2626c2
Jack <jackratner@gmail.com>
2021-07-21 11:15:54 -0400

    candidates and rounds API endpoints

--- api/api.rb
+++ api/api.rb
@@ -1,3 +1,5 @@
+require 'voting/voter'
+
 module Votes
   class API < Grape::API
     format :json
@@ -5,6 +7,26 @@ module Votes
     resource :ping do
       get do
         { hello: 'world' }
+      end
+    end
+
+    resource :candidates do
+      get do
+        Voting::Voter.new.candidates
+      end
+
+      get ':id' do
+        Voting::Voter.new.candidate(params[:id])
+      end
+    end
+
+    resource :rounds do
+      get do
+        Voting::Voter.new.rounds
+      end
+
+      get ':id' do
+        Voting::Voter.new.round(params[:id])
       end
     end
   end
